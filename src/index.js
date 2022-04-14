6371bd4574aa80b89a3acd5a2523761fb84ac08c
dpartner <d.partner.ua@gmail.com>
2022-04-14 17:09:45 +0300

    хедер

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './sass/main.scss';
+
+const ref = {
+  burgerButton: document.querySelector('.header__button'),
+  burgerMenu: document.querySelector('.header__burger-menu'),
+};
+
+ref.burgerButton.addEventListener('click', openMenu);
+
+function openMenu(e) {
+  e.currentTarget.classList.toggle('toggle-menu');
+  ref.burgerMenu.classList.toggle('open-menu');
+}
