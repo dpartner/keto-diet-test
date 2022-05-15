3a2c1c9d5b68e69fe7be876c01527122211a260e
dpartner <d.partner.ua@gmail.com>
2022-05-15 19:48:26 +0300

    start cookies

--- src/index.js
+++ src/index.js
@@ -15,11 +15,17 @@ const ref = {
   female: document.querySelector('.select-gender__wrap--female'),
   male: document.querySelector('.select-gender__wrap--male'),
   resetButton: document.querySelector('.reset-button'),
+  cookieWrap: document.querySelector('.cookie__wrap'),
 };
 
 ref.female.addEventListener('click', onFemale);
 ref.male.addEventListener('click', onMale);
 ref.resetButton.addEventListener('click', onResetLocalStorage);
+document.querySelector('#accept').addEventListener('click', onCookiesAccept);
+document.querySelector('#reject').addEventListener('click', onCookiesReject);
+document.querySelector('#read').addEventListener('click', onCookiesLink);
+
+onLoad();
 
 function onFemale() {
   const page = 1;
@@ -35,4 +41,38 @@ function onMale() {
 
 function onResetLocalStorage() {
   localStorage.clear();
+}
+
+function onCookiesAccept(e) {
+  e.preventDefault();
+  localStorage.setItem('cookies', true);
+  ref.cookieWrap.style.display = 'none';
+}
+
+async function onCookiesReject(e) {
+  const delay = 10000;
+  e.preventDefault();
+  localStorage.setItem('cookies', false);
+  ref.cookieWrap.style.display = 'none';
+  const timer = await timeOut(delay);
+  ref.cookieWrap.style.display = 'flex';
+}
+
+function onCookiesLink() {}
+
+function onLoad() {
+  if (localStorage.getItem('cookies') === 'true') {
+    console.log(localStorage.getItem('cookies'));
+    ref.cookieWrap.style.display = 'none';
+    console.log(1);
+  } else {
+    ref.cookieWrap.style.display = 'flex';
+    console.log(2);
+  }
+}
+
+function timeOut(delay) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(delay), delay);
+  });
 }
