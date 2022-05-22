420d89daaf863d7f117e350909ff41d38adbae85
dpartner <d.partner.ua@gmail.com>
2022-05-22 10:33:12 +0300

    chto tam

--- src/registration.js
+++ src/registration.js
@@ -1,5 +1,7 @@
 // import { ref } from './js/quiz-ref';
 import throttle from 'lodash.throttle';
+import php from './send.php';
+console.log(php);
 
 const ref = {
   buttonWrap: document.querySelector('.reg__button-wrap'),
@@ -61,6 +63,7 @@ function onSendReg(e) {
 
   if (email.match(patternEmail) && ref.regCheckbox.checked) {
     console.log(1);
+    send({ e, php });
   } else if (!email.match(patternEmail) && ref.regCheckbox.checked) {
     ref.inputNotification.textContent = 'This field is required.';
     ref.inputNotification.classList.add('active');
@@ -81,4 +84,38 @@ function onSendReg(e) {
 function onCheckbox() {
   ref.regCustomCheckbox.style.borderColor = '';
   ref.regCheckboxLabelText.style.borderBottom = '';
+}
+
+document.querySelector('#form').addEventListener('submit', send);
+
+// Отправка данных на сервер
+function send({ e: event, php }) {
+  console.log('Отправка запроса');
+  event.preventDefault ? event.preventDefault() : (event.returnValue = false);
+  var req = new XMLHttpRequest();
+  req.open('POST', php, true);
+  req.onload = function () {
+    if (req.status >= 200 && req.status < 400) {
+      json = JSON.parse(this.response); // Ебанный internet explorer 11
+      console.log(json);
+
+      // ЗДЕСЬ УКАЗЫВАЕМ ДЕЙСТВИЯ В СЛУЧАЕ УСПЕХА ИЛИ НЕУДАЧИ
+      if (json.result == 'success') {
+        // Если сообщение отправлено
+        alert('Сообщение отправлено');
+      } else {
+        // Если произошла ошибка
+        alert('Ошибка. Сообщение не отправлено');
+      }
+      // Если не удалось связаться с php файлом
+    } else {
+      alert('Ошибка сервера. Номер: ' + req.status);
+    }
+  };
+
+  // Если не удалось отправить запрос. Стоит блок на хостинге
+  req.onerror = function () {
+    alert('Ошибка отправки запроса');
+  };
+  req.send(new FormData(event.target));
 }
